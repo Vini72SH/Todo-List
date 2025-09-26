ea85b8276e70a90895973407d67075693de47e70
Vinícius Santos <viniciusjere@outlook.com>
2025-09-26 12:02:02 -0300

    Starting of Static Structure

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,9 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: "asset/resource",
+                generator: {
+                    filename: 'icons/[name][ext][query]'
+                }
             },
         ],
     },
