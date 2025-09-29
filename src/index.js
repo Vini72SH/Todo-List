8f170cc856d228e53e0fb047f078b92525490e55
Vinícius Santos <viniciusjere@outlook.com>
2025-09-29 11:43:15 -0300

    List of projects and modularization

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+import { addProject, showButton } from "./modules/projectsUI.js";
+
+const submitButton = document.querySelector("#submit");
+const newProjectButton = document.querySelector("#new-project-button");
+
+newProjectButton.addEventListener("click", () => {
+    showButton();
+});
+
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    addProject();
+});
---
+++ src/modules/projectsUI.js
@@ -0,0 +1,38 @@
+import closeImg from "../icons/close.svg"
+
+let active = false;
+const form = document.querySelector(".form");
+const projectsDiv = document.querySelector(".projects");
+
+export function showButton() {
+    if (active === false) {
+        form.setAttribute("style", "display: flex");
+        active = true;
+    }
+}
+
+export function addProject() {
+    const projectName = document.querySelector("#project-name").value.trim();
+    if (projectName) {
+        const close = document.createElement("img");
+        const closeButton = document.createElement("button");
+        const projectButton = document.createElement("button");
+
+        close.src = closeImg;
+        close.className = "close-img";
+
+        closeButton.className = "close-button";
+        closeButton.appendChild(close);
+
+        projectButton.textContent = projectName;
+        projectButton.className = "project-button";
+
+        projectButton.appendChild(closeButton);
+        projectsDiv.appendChild(projectButton);
+
+        form.setAttribute("style", "display: none");
+        active = false;
+    } else {
+        alert("Please enter a name for the project");
+    }
+}
